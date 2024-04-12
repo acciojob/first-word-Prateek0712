bc30a9ea8a43c5e54e41ae00e387e1ee94711b58
Prateek Jadhav <134202260+Prateek0712@users.noreply.github.com>
2024-04-12 10:23:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
   // your code here
+	s=s.trim();
+	let newStr="";
+	let i=0;
+	while(s[i]!=" ")
+		{
+			newStr+=s[i];
+			i++;
+		}
+	return newStr;
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+//const s = prompt("Enter String:");
 alert(firstWord(s));
